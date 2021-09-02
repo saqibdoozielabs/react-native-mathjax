4800a016ed912a7af51a9d5ea8767d4309847509
saqibdoozielabs <muhammad.saqib@doozielabs.com>
2021-09-02 20:31:45 +0500

    changes

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { View } from 'react-native';
-import { WebView } from 'react-native-webview';
+import WebView from 'react-native-autoheight-webview';
 
 const defaultOptions = {
 	messageStyle: 'none',
